6fdb609c51b4fdaf85079c88dfbb4141636388b9
Elena <elenanikolaevna999@ya.ru>
2021-12-20 14:35:07 +0300

    fix

--- bot.py
+++ bot.py
@@ -8,9 +8,11 @@ from handler import poem
 import time
 
 
+# файл логов
 logger = get_logger('app')
 
 
+# декоратор-обработчик ошибок
 def error_decor(func):
     def wrapper(*args, **kwargs):
         try:
@@ -21,28 +23,31 @@ def error_decor(func):
     return wrapper
 
 
+# тестовая функция для отправки времени в чат
 @error_decor
 def send_time(chat_id):
     bot.send_message(chat_id=chat_id, text=time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
 
+# отправка сообщения со стихотворениями в чат
 @error_decor
 def send_poem(chat_id, n=1):
     msg = poem(n)
     bot.send_message(chat_id=chat_id, text=msg, parse_mode='HTML')
 
 
-# Создаем экземпляр бота
+# Создаем экземпляр бота и планировщика
 bot = telebot.TeleBot(token=config.BOT_TOKEN)
 scheduler = BackgroundScheduler(timezone=config.SCHEDULER_TIMEZONE)
 
 
 # Функция, обрабатывающая команду /start
 @error_decor
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["start", "help"])
 def start(m, res=False):
     logger.log(LOG_LEVEL, f"New user: {m.chat.id} - {m.chat.username}")
 
+    # создание кнопок
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Now")
     item2 = types.KeyboardButton("Daily")
@@ -51,11 +56,13 @@ def start(m, res=False):
 
     bot.send_message(
         m.chat.id,
-        f'Я на связи {chr(config.emoji["hi"])}\n\t<b>Now</b> - получение стихотворения сейчас\n\t<b>Daily</b> — получение стихотворения каждый день',
-        reply_markup=markup
+        f'Я на связи {chr(config.emoji["hi"])}\n\t\t<b>Now</b> - получение стихотворения сейчас\n\t\t<b>Daily</b> — получение стихотворения каждый день',
+        reply_markup=markup,
+        parse_mode='HTML'
     )
 
 
+# Функция, обрабатывающая команду /daily
 @error_decor
 @bot.message_handler(commands=["daily"])
 def daily(m, res=False):
@@ -69,6 +76,7 @@ def daily(m, res=False):
     )
 
 
+# Функция, обрабатывающая команду /now
 @error_decor
 @bot.message_handler(commands=["now"])
 def now(message, res=False):
@@ -78,6 +86,7 @@ def now(message, res=False):
         send_poem(chat_id=message.chat.id)
 
 
+# Функция, обрабатывающая любой присланный пользователем текст
 @error_decor
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -88,6 +97,7 @@ def handle_text(message):
         daily(message)
 
 
+# Запуск экземпляр бота и планировщика
 if __name__ == '__main__':
     scheduler.start()
     bot.polling(none_stop=True, interval=0)
